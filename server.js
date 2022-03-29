1fca332d80b44afe07aa1a7b3863b48c59a00ac0
Sharan AJ <sharanajs2001@gmail.com>
2022-03-29 13:51:19 +0530

    Ejs and rendering dynamic data in express app

--- server.js
+++ server.js
@@ -3,24 +3,26 @@ const express = require("express");
 // express app
 const app = express();
 const PORT = 4000;
-
+// register view
+app.set("view engine", "ejs");
 // GET /
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
+  // res.sendFile("./views/index.html", { root: __dirname });
+  res.render("index", { title: "Express App | Home" });
 });
 // GET /about
-app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
-});
+// * app.get("/about", (req, res) => {
+// *  res.sendFile("./views/about.html", { root: __dirname });
+// * });
 // GET /aboutus
-app.get("/aboutus", (req, res) => {
-  res.redirect("/about");
-});
+// * app.get("/aboutus", (req, res) => {
+// * res.redirect("/about");
+// * });
 // GET 404
 app.use((req, res) => {
   res.status(404).send("Not found 😕");
 });
 // starting the server
 app.listen(PORT, () => {
-  console.log("Server starting at http://localhost:4000");
+  console.log("Server running at http://localhost:4000");
 });
